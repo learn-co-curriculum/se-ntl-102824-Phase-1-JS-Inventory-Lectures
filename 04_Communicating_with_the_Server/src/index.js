5cad20e02b295832411877735101f1cb831af5b5
betalantz <betalantz@gmail.com>
2024-10-31 13:09:39 -0700

    end lecture 04

--- 04_Communicating_with_the_Server/src/index.js
+++ 04_Communicating_with_the_Server/src/index.js
@@ -1,86 +1,134 @@
+////////////////////
+// global selectors
+////////////////////
+const toggleBookFormButton = document.querySelector("#toggleForm");
+const bookForm = document.querySelector("#book-form");
+
+///////////////////
+// helper functions
+///////////////////
+
 function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
+  return "$" + Number.parseFloat(price).toFixed(2);
 }
+
+function fillIn(form, data) {
+  form.title.value = data.title;
+  form.author.value = data.author;
+  form.price.value = data.price;
+  form.imageUrl.value = data.imageUrl;
+  form.inventory.value = data.inventory;
+}
+
+fillIn(bookForm, {
+  title: "Designing Data-intensive Applications",
+  author: "Martin Kleppmann",
+  price: 22,
+  inventory: 1,
+  imageUrl:
+    "https://m.media-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg",
+});
 
 ///////////////////
 // render functions
 ///////////////////
 function renderHeader(bookStore) {
-  document.querySelector('#store-name').textContent = bookStore.name;
+  document.querySelector("#store-name").textContent = bookStore.name;
 }
 
 function renderFooter(bookStore) {
-  document.querySelector('#address').textContent = bookStore.address;
-  document.querySelector('#number').textContent = bookStore.number;
-  document.querySelector('#store').textContent = bookStore.location;
+  document.querySelector("#address").textContent = bookStore.address;
+  document.querySelector("#number").textContent = bookStore.number;
+  document.querySelector("#store").textContent = bookStore.location;
 }
 
-// function: renderBook(book)
-// --------------------------
-// accepts a book object as an argument and creates
-// an li something like this:
-// <li class="list-li">
-//   <h3>Eloquent JavaScript</h3>
-//   <p>Marjin Haverbeke</p>
-//   <p>$10.00</p>
-//   <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
-// </li>
-// appends the li to the ul#book-list in the DOM
+function renderAllBooks(inventory) {
+  // bookStore.inventory.forEach(book => renderBook(book));
+  inventory.forEach(renderBook); // this syntax is shorthand for line above
+}
+
 function renderBook(book) {
-    
-  const li = document.createElement('li');
-  li.className = 'list-li';
-  
-  const h3 = document.createElement('h3');
+  const li = document.createElement("li");
+  li.className = "list-li";
+
+  const h3 = document.createElement("h3");
   h3.textContent = book.title;
   li.append(h3);
 
-  const pAuthor = document.createElement('p');
+  const pAuthor = document.createElement("p");
   pAuthor.textContent = book.author;
   li.append(pAuthor);
-  
-  const pPrice = document.createElement('p');
+
+  const pPrice = document.createElement("p");
   pPrice.textContent = formatPrice(book.price);
   li.append(pPrice);
-  
-  const pStock = document.createElement('p');
+
+  const pStock = document.createElement("p");
   pStock.className = "grey";
   if (book.inventory === 0) {
     pStock.textContent = "Out of stock";
   } else if (book.inventory < 3) {
     pStock.textContent = "Only a few left!";
   } else {
-    pStock.textContent = "In stock"
+    pStock.textContent = "In stock";
   }
   li.append(pStock);
-  
-  const img = document.createElement('img');
+
+  const img = document.createElement("img");
   img.src = book.imageUrl;
   img.alt = `${book.title} cover`;
   li.append(img);
 
-  const btn = document.createElement('button');
-  btn.textContent = 'Delete';
+  const btn = document.createElement("button");
+  btn.textContent = "Delete";
   li.append(btn);
 
-  btn.addEventListener('click', (e) => {
+  btn.addEventListener("click", (e) => {
     li.remove();
-  })
+  });
 
-
-  document.querySelector('#book-list').append(li);
+  document.querySelector("#book-list").append(li);
 }
 
+///////////////////////////////////////////////
+// Comm w/ server (via .fetch) -> update DOM
+///////////////////////////////////////////////
+
+fetch("http://localhost:3000/books")
+  .then((response) => {
+    console.dir(response);
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`message: ${response.statusText}`);
+    }
+  })
+  .then((booksArr) => {
+    console.log(booksArr);
+    renderAllBooks(booksArr);
+  })
+  .catch((err) => console.error(err));
+
+fetch("http://localhost:3000/stores")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(`message: ${res.statusText}`);
+    }
+  })
+  .then((storesArr) => {
+    renderHeader(storesArr[0]);
+    renderFooter(storesArr[0]);
+  })
+  .catch((err) => console.error(err));
 
 ////////////////////////////////////////////////////////////////
 // Event Listeners/Handlers (Behavior => Data => Display)
 ////////////////////////////////////////////////////////////////
 
-const toggleBookFormButton = document.querySelector('#toggleForm')
-const bookForm = document.querySelector('#book-form');
-
 function toggleBookForm() {
-  const bookFormHidden = bookForm.classList.toggle('collapsed');
+  const bookFormHidden = bookForm.classList.toggle("collapsed");
   if (bookFormHidden) {
     toggleBookFormButton.textContent = "New Book";
   } else {
@@ -89,21 +137,21 @@ function toggleBookForm() {
 }
 
 // hide and show the new book form when toggle buton is clicked
-toggleBookFormButton.addEventListener('click', (e) => {
+toggleBookFormButton.addEventListener("click", (e) => {
   toggleBookForm();
 });
 
 // also hide the form when it's visible and the escape key is pressed
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    if (!bookForm.classList.contains('collapsed')) {
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    if (!bookForm.classList.contains("collapsed")) {
       toggleBookForm();
     }
   }
-})
+});
 
 // handle submitting new book form
-bookForm.addEventListener('submit', (e) => {
+bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const book = {
@@ -112,23 +160,40 @@ bookForm.addEventListener('submit', (e) => {
     price: parseFloat(e.target.price.value),
     inventory: parseInt(e.target.inventory.value),
     imageUrl: e.target.imageUrl.value,
-    reviews: []
-  }
-  
+    reviews: [],
+  };
+
   e.target.reset(); // clear form
   toggleBookForm(); // hide book form
   renderBook(book); // display new book to DOM
-})
-
-
+});
 
 ////////////////////////////////////////////
 // call render functions to populate the DOM
 ////////////////////////////////////////////
 
-renderHeader(bookStore)
-renderFooter(bookStore)
-bookStore.inventory.forEach(renderBook)
+// renderHeader(bookStore)
+// renderFooter(bookStore)
+// renderAllBooks(bookStore)
 
+console.dir(window);
 
+////////////////////////////
+// another example
+///////////////////////////
 
+let requestedTodo; // declaring a variable in global scope w/o assignment
+console.log("🚀 ~ requestedTodo:", requestedTodo);
+
+const result = fetch("https://jsonplaceholder.typicode.com/todos/2") // fetch returns a Promise
+  .then((response) => {
+    // Promise result is a Response obj
+    console.log(response);
+    return response.json(); // .json() returns a Promise
+  })
+  .then((todoJSON) => {
+    // Promise result is JSON
+    requestedTodo = todoJSON; // assign result to global variable to get data out of this callback
+    console.log("🚀 ~ requestedTodo:", requestedTodo);
+  });
+console.log("🚀 ~ result:", result);
